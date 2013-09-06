a704b898bcf24cacb7f2caf7fffe8a98e4633bd2
Chad Wilken <chad.wilken@gmail.com>
2013-09-05 22:30:02 -0500

    Initial commit

---
+++ flyto.js
@@ -0,0 +1,30 @@
+(function ($) {
+  $.fn.flyTo = function(to, options) {
+    var $this = this,
+    defaults = {
+      opacity: 250,
+      speed: 1000,
+      fadeOutSpeed: 150
+    };
+
+    var opts = $.extend({}, defaults, options);
+
+
+    var start = $this.offset();
+    var newFly = $this.clone().css({position: 'fixed', left: start.left, top: start.top, width: $this.width(), height: $this.height()}).fadeTo(0, opts.opacity).appendTo('body');
+    var end = to.offset();
+    
+    newFly.animate({
+      top: (end.top - $(window).scrollTop()),
+      left: end.left
+    }, opts.speed, function(){
+      $this.trigger('hide')
+      newFly.fadeTo(opts.fadeOutSpeed,0, function(){
+        newFly.remove();
+        $this.trigger('hidden')
+      });
+    });
+
+    return $this;
+  };
+})(jQuery);
